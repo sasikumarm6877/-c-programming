a894e3573be415353baf0223b621d675f1892ffd
SASIKUMAR M <sasikumarm6877@gmail.com>
2025-07-06 14:17:27 +0000

    Update bitwise.c

--- bitwise.c
+++ bitwise.c
@@ -43,6 +43,46 @@ num=num^1<<j;
 }
 printf("%d\n",num);
 }
+//bit 1.) set or 2.)clear or 3.) compliment
+#include<stdio.h>
+int main()
+int num,pos,op,c=0;
+printf("Enter the number :\n");
+scanf("%d",&num);
+L1:
+printf("Enter the bit pos(0-31)\n");
+scanf("%d",&pos);
+if(pos>=0&&pos<=31)
+{
+printf("Enter the op\n1)set\n2)clear\n3)complimant\n");
+scanf("%d",&op);
+if(op==1)
+{
+num=num|1<<pos;
+printf("num=%d\n",num);
+}
+else if(op==2)
+{
+num=num&~(1<<pos);
+printf("num=%d\n",num);
+}
+else if(op==3)
+{
+num=num^1<<pos;
+printf("num=%d\n",num);
+}
+lse
+printf("Wrong option\n");
+}
+else
+{
+c++;                                                                                                   1,1         Top
+printf("Wrong bit pos as input\n");
+if(c<3);
+goto L1;
+}
+}
+
 
 
 
